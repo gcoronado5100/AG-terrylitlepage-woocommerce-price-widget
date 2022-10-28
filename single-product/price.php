fa567d92fd45889305d6b6dba3f7eabb07da84b3
Gabriel Coronado <74467208+gcoronado5100@users.noreply.github.com>
2022-10-28 14:33:41 -0400

    add files

---
+++ single-product/price.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * Single Product Price
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/price.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.0.0
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+global $product;
+$siteURL = site_url();
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+    CURLOPT_URL => "$siteURL/wp-json/wholesale/v1/products/$product->id",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => [
+        "Accept: */*",
+        "Authorization: Basic Y2tfYmJkZDkzOTQ5YzU2OGExYmYxZmVmMjgzYjc1MDI4YTcxNTBjZGYyYzpjc19kMmRmMDZlMGQ2NjAwZjdlNjY5ZDU2MGVkNjk5YzJkMjk0ZTU5M2Rj",
+        "User-Agent: Thunder Client (https://www.thunderclient.com)"
+    ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    $productDetails = json_decode($response);
+}
+
+
+function ag_getProductVariations($siteURL, $product)
+{
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+        CURLOPT_URL => "$siteURL/wp-json/wholesale/v1/products/$product->id/variations",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "Accept: */*",
+            "Authorization: Basic Y2tfYmJkZDkzOTQ5YzU2OGExYmYxZmVmMjgzYjc1MDI4YTcxNTBjZGYyYzpjc19kMmRmMDZlMGQ2NjAwZjdlNjY5ZDU2MGVkNjk5YzJkMjk0ZTU5M2Rj",
+            "User-Agent: Thunder Client (https://www.thunderclient.com)"
+        ],
+    ]);
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        $productDetailsVariation = json_decode($response);
+    }
+    $productDiscount = $productDetailsVariation[0]->wholesale_data->wholesale_price->wholesale_customer;
+
+    return  $productDiscount;
+}
+
+?>
+
+
+<?php if ($price_html = $product->get_price_html()) : ?>
+    <?php $user = wp_get_current_user();
+
+    if (!in_array('wholesale_customer', (array) $user->roles)) : ?>
+
+
+        <div class="price__container">
+            <span><strong>Member's Price</strong></span>
+
+            <?php if ($productDetails->variations != NULL) : ?>
+                <span class="price"><?= "$" . ag_getProductVariations($siteURL, $product); ?></span>
+
+            <?php elseif ($productDetails->wholesale_data->wholesale_price->wholesale_customer != null) : ?>
+                <span class="price"><?= $productDetails->wholesale_data->wholesale_price->wholesale_customer ?></span>
+            <?php else :
+                $member_role_price_rule =  round(floatval(get_option(WWPP_OPTION_WHOLESALE_ROLE_GENERAL_DISCOUNT_MAPPING, array([]))["wholesale_customer"]), 2, PHP_ROUND_HALF_UP);
+            ?>
+                <span class="price"><?= '$ ' . round(($product->price - ($product->price * ($member_role_price_rule / 100))), 2, PHP_ROUND_HALF_UP); ?></span>
+            <?php endif; ?>
+            <span><strong>Regular Price</strong></span>
+            <span class="price"><?= wp_kses_post($price_html); ?></span>
+        </div>
+
+    <?php else : ?>
+
+        <span class="price"><?php echo wp_kses_post($price_html); ?></span>
+    <?php endif; ?>
+
+
+<?php endif; ?>
